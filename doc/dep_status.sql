8d3245cf7f5059d71a51bfbca5c1199f06291257
Josh Riley <joshjriley@gmail.com>
2020-10-21 12:40:44 -1000

    updated with new sql defs

--- doc/dep_status.sql
+++ doc/dep_status.sql
@@ -1,34 +1,37 @@
+
 CREATE TABLE IF NOT EXISTS `dep_status` (
-  `id`            int(11)       NOT NULL        AUTO_INCREMENT PRIMARY KEY,      
-  `instr`         varchar(16)   NOT NULL                        COMMENT 'Instrument name',      
-  `filepath`      text          NOT NULL                        COMMENT 'Original file input location',      
-  `koaid`         varchar(24)   DEFAULT NULL    UNIQUE          COMMENT 'Unique KOAID',     
-  `utc_datetime`  datetime      DEFAULT NULL                    COMMENT 'Datetime of image (utdate + utc)',      
-  `raw_savepath`  text          DEFAULT NULL                    COMMENT 'Local archived raw file location',     
-  `arch_stat`     varchar(16)   DEFAULT NULL                    COMMENT 'Overall archive status [NULL, QUEUED, PROCESSING, INVALID, DONE, ERROR]',  
-  `file_valid`    varchar(32)   DEFAULT NULL                    COMMENT '[YES, EMPTY, UNREADABLE, DUPLICATE, etc]',   
-  `file_size`     double        DEFAULT NULL                    COMMENT 'FITS size in MB',  
-  `arch_size`     double        DEFAULT NULL                    COMMENT 'Archive size (including ancillary files)',  
-  `sdata_dir`     varchar(8)    DEFAULT NULL                    COMMENT 'Numerical sdata dir suffix (ie 904, 401)',  
-  `creation_time` datetime      DEFAULT NULL                    COMMENT 'Time record created',      
-  `arch_time`     datetime      DEFAULT NULL                    COMMENT 'Time file ready for transfer',  
-  `xfr_time`      datetime      DEFAULT NULL                    COMMENT 'Time files tranferred to IPAC', 
-  `tpx_time`      datetime      DEFAULT NULL                    COMMENT 'Time of IPAC response', 
-  `tpx_stat`      varchar(32)   DEFAULT NULL                    COMMENT 'IPAC response [ERROR, etc]', 
-  `image_type`    varchar(32)   DEFAULT NULL                    COMMENT '[Science, calibration, etc]',   
-  `semid`         varchar(10)   DEFAULT NULL                    COMMENT 'Semester + ProgID',   
-  `header_json`   json          DEFAULT NULL                    COMMENT 'Store all FITS header info as json',    
-  `last_mod`      timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `koaid`               varchar(30)   PRIMARY KEY   COMMENT 'Unique KOA ID',
+  `instrument`          varchar(15)   NOT NULL      COMMENT 'Instrument name',
+  `utdatetime`          datetime      NOT NULL      COMMENT 'DATE-OBS UTC',
+  `status`              varchar(15)                 COMMENT 'Current status of archive process [QUEUED, PROCESSING, COMPLETE, INVALID, ERROR]',
+  `status_code`         varchar(25)                 COMMENT 'Status code of archive process [NULL, DUPLICATE, EMPTY, UNREADABLE, etc]',
+  `ofname`              varchar(255)                COMMENT 'Full path to original file (sdata location)',
+  `stage_file`          varchar(255)                COMMENT 'Full path the staged original raw file',
+  `archive_dir`         varchar(255)                COMMENT 'Directory file is archived',
+  `creation_time`       datetime                    COMMENT 'Date and time the FITS file is ready to be processed',
+  `dep_start_time`      datetime                    COMMENT 'Date and time that DEP processing started',
+  `dep_end_time`        datetime                    COMMENT 'Date and time that file processing is complete',
+  `xfr_start_time`      datetime                    COMMENT 'Date and time that transfer started',
+  `xfr_end_time`        datetime                    COMMENT 'Date and time that transfer is complete',
+  `ipac_notify_time`    datetime                    COMMENT 'Date and time that IPAC is notified to start ingestion',
+  `ipac_response_time`  datetime                    COMMENT 'Date and time that IPAC ingestion response received',
+  `stage_time`          datetime                    COMMENT 'Date and time that original file copied to stage directory',
+  `filesize_mb`         double                      COMMENT 'FITS file size in megabytes',
+  `archsize_mb`         double                      COMMENT 'Size of complete FITS dataset in megabytes',
+  `koaimtyp`            varchar(20)                 COMMENT 'Image type of the FITS file',
+  `semid`               varchar(15)                 COMMENT 'SEMID of FITS file association',
+  `last_mod`            timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 
-CREATE TABLE IF NOT EXISTS `dep_error` (
-  `id`            int(11)       NOT NULL        AUTO_INCREMENT PRIMARY KEY,      
-  `instr`         varchar(16)   DEFAULT NULL                    COMMENT 'Instrument name',      
-  `code`          varchar(32)   DEFAULT NULL                    COMMENT 'Error code string',   
-  `info`          text          DEFAULT NULL                    COMMENT 'Text description of error',      
-  `script`        varchar(256)  DEFAULT NULL                    COMMENT 'Script reporting the error',  
-  `status`        varchar(10)   DEFAULT NULL                    COMMENT '[RESOLVED, DELETED]',   
-  `creation_time` datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP,      
+CREATE TABLE `dep_status_history` like `dep_status`;
+alter table dep_status_history modify column `koaid` varchar(30) COMMENT 'KOA ID';
+alter table dep_status_history drop primary key FIRST;
+alter table dep_status_history add column `id`  int(11)  NOT NULL  AUTO_INCREMENT PRIMARY KEY before `koaid`;
+
+
+CREATE TABLE IF NOT EXISTS `headers` (
+  `koaid`         varchar(30)   PRIMARY KEY         COMMENT 'Unique KOA ID',
+  `header`        json          DEFAULT NULL        COMMENT 'Store all FITS header info as json',    
   `last_mod`      timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
