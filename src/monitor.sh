c883ed83a7891b59de42a609b099b5458c766c72
Josh Riley <joshjrley@gmail.com>
2020-10-29 13:33:18 -1000

    updates for koarti accnt

--- src/monitor.sh
+++ src/monitor.sh
@@ -4,11 +4,11 @@
 source $HOME/.cshrc
 
 set PYTHON='/usr/local/anaconda/bin/python'
-set DEPDIR='/home/jriley/dev/koa/dep-rti'
+set DEPDIR='/usr/local/home/koarti/dev/koa/dep-rti'
 set LOGFILE='/koadata/dep-rti.log'
 
 #todo: normally we would do start, not a restart, but using restart for development
-#$PYTHON $DEPDIR/src/manager.py monitor start --extra "kcwi" >> $LOGFILE
+#$PYTHON $DEPDIR/src/manager.py monitor start --extra "kcwi hires nires" >> $LOGFILE
 
-$PYTHON $DEPDIR/src/manager.py monitor restart --extra "kcwi" >>& $LOGFILE
-#$PYTHON $DEPDIR/src/manager.py monitor restart --extra "kcwi"
+$PYTHON $DEPDIR/src/manager.py monitor restart --extra "kcwi hires nires" >>& $LOGFILE
+#$PYTHON $DEPDIR/src/manager.py monitor restart --extra "kcwi hires nires"
