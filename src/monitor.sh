77d67a9940c969aa2caf1d9cd99d4d3bcb606367
Josh Riley <joshjriley@gmail.com>
2021-02-23 09:47:54 -1000

    Change to monitor one service per monitor instance.

--- src/monitor.sh
+++ src/monitor.sh
@@ -1,16 +1,19 @@
 #!/usr/local/bin/tcsh
-set instr=$argv[1]
-set instrupper=`echo "$instr" | tr '[a-z]' '[A-Z]'`
 
 #source environment variables so script will work from cron
 source $HOME/.cshrc
 
-set PYTHON='/usr/local/anaconda/bin/python'
-set DEPDIR=`dirname $0`
-set LOGFILE="/koadata/$instrupper/dep-rti-$instr.log"
+#loop services
+foreach service ( $argv )
 
-#todo: normally we would do start, not a restart, but using restart for development
-#$PYTHON $DEPDIR/src/manager.py monitor start --extra "$instr" >> $LOGFILE
-set cmd="$PYTHON $DEPDIR/manager.py monitor restart --extra $instrupper >>& $LOGFILE"
-echo $cmd
-eval "$cmd"
+	set PYTHON='/usr/local/anaconda/bin/python'
+	set DEPDIR=`dirname $0`
+	set LOGFILE="/koadata/dep-rti-$service.log"
+
+	#todo: normally we would do start, not a restart, but using restart for development
+	#$PYTHON $DEPDIR/src/manager.py monitor start --extra "$service" >> $LOGFILE
+	set cmd="$PYTHON $DEPDIR/manager.py monitor restart --extra $service >>& $LOGFILE"
+	echo $cmd
+	eval "$cmd"
+
+end
--- src/monitor_config.py
+++ src/monitor_config.py
@@ -8,153 +8,147 @@ Define instrument keywords to monitor that indicate a new datafile was written.
   (NOTE: 'probe' must be a different keyword than 'trigger' b/c probe() read causes callback)
 '''
 #todo: This could be put in each of the instr subclasses.
+
+
+
 instr_keymap = {
-    'KCWI': [
-        {
-            'service'  :  'kfcs',
-            'trigger'  :  'LASTFILE',
-            'val'      :  None,
-            'fp_info'  :  ['LASTFILE'],
-            'format'   :  lambda vals: f"{vals['LASTFILE']}",
-            'heartbeat':  ['ITERATION', 30]
-        },
-        {
-            'service'  :  'kbds',
-            'trigger'  :  'LOUTFILE',
-            'val'      :  None,
-            'fp_info'  :  ['LOUTFILE'],
-            'format'   :  lambda vals: f"{vals['LOUTFILE']}",
-            'heartbeat': ['ITERATION', 1]
-        }
-    ],
-    'NIRES': [
-        {
-            'service'  :  'nids',
-            'trigger'  :  'LASTFILE',
-            'val'      :  None,
-            'fp_info'  :  ['LASTFILE'],
-            'format'   :  lambda vals: f"{vals['LASTFILE']}",
-            'heartbeat':  ['ITERATION', 600]
-        },
-        {
-            'service'  :  'nsds',
-            'trigger'  :  'LASTFILE',
-            'val'      :  None,
-            'fp_info'  :  ['LASTFILE'],
-            'format'   :  lambda vals: f"{vals['LASTFILE']}",
-            'heartbeat':  ['ITERATION', 600]
-        }
-    ],
-    'DEIMOS': [
-        {
-            'service'  :  'deimosplus',
-            'trigger'  :  'LASTCCD',
-            'val'      :  None,
-            'fp_info'  :  ['LASTCCD'],
-            'format'   :  lambda vals: f"{vals['LASTCCD']}",
-            'heartbeat':  ['UPTIME', 1]
-        },
-        {
-            'service'  :  'deifcs',
-            'trigger'  :  'FCSIMGFI',
-            'val'      :  None,
-            'fp_info'  :  ['FCSIMGFI'],
-            'format'   :  lambda vals: f"{vals['FCSIMGFI']}",
-            'heartbeat':  '' # there is no keyword to track that this service is up
-        }
-    ],
-    'ESI': [
-        {
-            'service'  :  'esi',
-            'trigger'  :  'WDISK',
-            'val'      :  'false',
-            'fp_info'  :  ['OUTDIR','OUTFILE','LFRAMENO'],
-            'format'   :  lambda vals: f"{vals['OUTDIR']}/{vals['OUTFILE']}{vals['LFRAMENO']:0>4}.fits",
-            'heartbeat':  '' # there is no keyword to track that this service is up
-        }
-    ],
-    'HIRES': [
-        {
-            'service'  :  'hiccd',
-            'trigger'  :  'WDISK',
-            'val'      :  'false',
-            'fp_info'  :  ['OUTDIR','OUTFILE','LFRAMENO'],
-            'format'   :  lambda vals: f"{vals['OUTDIR']}/{vals['OUTFILE']}{vals['LFRAMENO']:0>4}.fits",
-            'heartbeat':  ['INFOMCLK', 1]
-        }
-    ],
-    'LRIS': [
-        {
-            'service'  :  'lris',
-            'trigger'  :  'WDISK',
-            'val'      :  'false',
-            'fp_info'  :  ['OUTDIR','OUTFILE','LFRAMENO'],
-            'format'   :  lambda vals: f"{vals['OUTDIR']}/{vals['OUTFILE']}{vals['LFRAMENO']:0>4}.fits",
-            'heartbeat':  '' # LRIS doesnt have any keywords to track uptime. LRIS upgrade should fix this
-        },
-        {
-            'service'  :  'lrisblue',
-            'trigger'  :  'WDISK',
-            'val'      :  'false',
-            'fp_info'  :  ['OUTDIR','OUTFILE','LFRAMENO'],
-            'format'   :  lambda vals: f"{vals['OUTDIR']}/{vals['OUTFILE']}{vals['LFRAMENO']:0>4}.fits",
-            'probe':  '' # LRIS doesnt have any keywords to track uptime. LRIS upgrade should fix this
-        }
-    ],
-    'MOSFIRE': [
-        {
-            'service'  :  'mosfire',
-            'trigger'  :  'LASTFILE',
-            'val'      :  None,
-            'fp_info'  :  ['LASTFILE'],
-            'format'   :  lambda vals: f"{vals['LASTFILE']}",
-            'heartbeat':  'LASTALIVE'
-        }
-    ],
-    'NIRC2': [
-        {
-            'service'  :  'alad',
-            'trigger'  :  'LASTFILE', # could alternatively be FILERDY with val==0
-            'val'      :  None,
-            'fp_info'  :  ['OUTDIR', 'LASTFILE'],
-            'format'   :  lambda vals: f"{vals['OUTDIR']}{vals['LASTFILE']}",
-            'heartbeat':  ''  # alad doesn't have any keyword to track how long it's been up
-        }
-    ],
-    'NIRSPEC': [
-        {
-            'service'  :  'nspec',
-            'trigger'  :  'LASTFILE',
-            'val'      :  None,
-            'fp_info'  :  ['LASTFILE'],
-            'format'   :  lambda vals: f"{vals['LASTFILE']}",
-            'heartbeat':  'UPTIME'
-        },
-        {
-            'service'  :  'nscam',
-            'trigger'  :  'LASTFILE',
-            'val'      :  None,
-            'fp_info'  :  ['LASTFILE'],
-            'format'   :  lambda vals: f"{vals['LASTFILE']}",
-            'heartbeat':  'UPTIME'
-        }
-    ],
-    'OSIRIS': [
-        {
-            'service'  :  'osiris',
-            'trigger'  :  'ILASTFILE',
-            'val'      :  None,
-            'fp_info'  :  ['ILASTFILE'],
-            'format'   :  lambda vals: f"{vals['ILASTFILE']}",
-            'heartbeat':  'LASTALIVE'
-        },
-        {
-            'service'  :  'osiris',
-            'trigger'  :  'SLASTFILE',
-            'val'      :  None,
-            'fp_info'  :  ['SLASTFILE'],
-            'format'   :  lambda vals: f"{vals['SLASTFILE']}",
-            'heartbeat':  'LASTALIVE'
-        }
-    ]
+
+    'kfcs': {
+        'instr'    :  'KCWI',
+        'trigger'  :  'LASTFILE',
+        'val'      :  None,
+        'fp_info'  :  ['LASTFILE'],
+        'format'   :  lambda vals: f"{vals['LASTFILE']}",
+        'heartbeat':  ['ITERATION', 30]
+    },
+    'kbds': {
+        'instr'    :  'KCWI',
+        'trigger'  :  'LOUTFILE',
+        'val'      :  None,
+        'fp_info'  :  ['LOUTFILE'],
+        'format'   :  lambda vals: f"{vals['LOUTFILE']}",
+        'heartbeat': ['ITERATION', 1]
+    },
+
+    'nids': {
+        'instr'    :  'NIRES',
+        'trigger'  :  'LASTFILE',
+        'val'      :  None,
+        'fp_info'  :  ['LASTFILE'],
+        'format'   :  lambda vals: f"{vals['LASTFILE']}",
+        'heartbeat':  ['ITERATION', 600]
+    },
+    'nsds': {
+        'instr'    :  'NIRES',
+        'trigger'  :  'LASTFILE',
+        'val'      :  None,
+        'fp_info'  :  ['LASTFILE'],
+        'format'   :  lambda vals: f"{vals['LASTFILE']}",
+        'heartbeat':  ['ITERATION', 600]
+    },
+
+    'deimosplus': {
+        'instr'    :  'DEIMOS',
+        'trigger'  :  'LASTCCD',
+        'val'      :  None,
+        'fp_info'  :  ['LASTCCD'],
+        'format'   :  lambda vals: f"{vals['LASTCCD']}",
+        'heartbeat':  ['UPTIME', 1]
+    },
+    'deifcs': {
+        'instr'    :  'DEIMOS',
+        'trigger'  :  'FCSIMGFI',
+        'val'      :  None,
+        'fp_info'  :  ['FCSIMGFI'],
+        'format'   :  lambda vals: f"{vals['FCSIMGFI']}",
+        'heartbeat':  '' # there is no keyword to track that this service is up
+    },
+
+    'esi': {
+        'instr'    :  'ESI',
+        'trigger'  :  'WDISK',
+        'val'      :  'false',
+        'fp_info'  :  ['OUTDIR','OUTFILE','LFRAMENO'],
+        'format'   :  lambda vals: f"{vals['OUTDIR']}/{vals['OUTFILE']}{vals['LFRAMENO']:0>4}.fits",
+        'heartbeat':  '' # there is no keyword to track that this service is up
+    },
+
+    'hiccd': {
+        'instr'    :  'HIRES',
+        'trigger'  :  'WDISK',
+        'val'      :  'false',
+        'fp_info'  :  ['OUTDIR','OUTFILE','LFRAMENO'],
+        'format'   :  lambda vals: f"{vals['OUTDIR']}/{vals['OUTFILE']}{vals['LFRAMENO']:0>4}.fits",
+        'heartbeat':  ['INFOMCLK', 1]
+    },
+
+    'lris': {
+        'instr'    :  'LRIS',
+        'trigger'  :  'WDISK',
+        'val'      :  'false',
+        'fp_info'  :  ['OUTDIR','OUTFILE','LFRAMENO'],
+        'format'   :  lambda vals: f"{vals['OUTDIR']}/{vals['OUTFILE']}{vals['LFRAMENO']:0>4}.fits",
+        'heartbeat':  '' # LRIS doesnt have any keywords to track uptime. LRIS upgrade should fix this
+    },
+    'lrisblue': {
+        'instr'    :  'lris',
+        'trigger'  :  'WDISK',
+        'val'      :  'false',
+        'fp_info'  :  ['OUTDIR','OUTFILE','LFRAMENO'],
+        'format'   :  lambda vals: f"{vals['OUTDIR']}/{vals['OUTFILE']}{vals['LFRAMENO']:0>4}.fits",
+        'probe':  '' # LRIS doesnt have any keywords to track uptime. LRIS upgrade should fix this
+    },
+
+    'mosfire': {
+        'instr'    :  'MOSFIRE',
+        'trigger'  :  'LASTFILE',
+        'val'      :  None,
+        'fp_info'  :  ['LASTFILE'],
+        'format'   :  lambda vals: f"{vals['LASTFILE']}",
+        'heartbeat':  'LASTALIVE'
+    },
+
+    'alad': {
+        'instr'    :  'NIRC2',
+        'trigger'  :  'LASTFILE', # could alternatively be FILERDY with val==0
+        'val'      :  None,
+        'fp_info'  :  ['OUTDIR', 'LASTFILE'],
+        'format'   :  lambda vals: f"{vals['OUTDIR']}{vals['LASTFILE']}",
+        'heartbeat':  ''  # alad doesn't have any keyword to track how long it's been up
+    },
+
+    'nspec': {
+        'instr'    :  'NIRSPEC',
+        'trigger'  :  'LASTFILE',
+        'val'      :  None,
+        'fp_info'  :  ['LASTFILE'],
+        'format'   :  lambda vals: f"{vals['LASTFILE']}",
+        'heartbeat':  'UPTIME'
+    },
+    'nscam': {
+        'instr'    :  'NIRSPEC',
+        'trigger'  :  'LASTFILE',
+        'val'      :  None,
+        'fp_info'  :  ['LASTFILE'],
+        'format'   :  lambda vals: f"{vals['LASTFILE']}",
+        'heartbeat':  'UPTIME'
+    },
+
+    'osiris': {
+        'instr'    :  'OSIRIS',
+        'trigger'  :  'ILASTFILE',
+        'val'      :  None,
+        'fp_info'  :  ['ILASTFILE'],
+        'format'   :  lambda vals: f"{vals['ILASTFILE']}",
+        'heartbeat':  'LASTALIVE'
+    },
+    #???
+    'osirisXXX': { 
+        'instr'    :  'OSIRIS',
+        'trigger'  :  'SLASTFILE',
+        'val'      :  None,
+        'fp_info'  :  ['SLASTFILE'],
+        'format'   :  lambda vals: f"{vals['SLASTFILE']}",
+        'heartbeat':  'LASTALIVE'
+    }
 }
