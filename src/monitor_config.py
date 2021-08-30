3d28acaf34591754f110753af96f202251a7b2fe
Jeff Mader <jmader@vm-koaserver2.(none)>
2021-08-30 12:12:48 -1000

    Update lredccd

--- src/monitor_config.py
+++ src/monitor_config.py
@@ -83,12 +83,10 @@ instr_keymap = {
         'transfer' : 1
     },
 
-    'lris': {
-        'instr'    :  'LREDCCD',
+    'lredccd': {
+        'instr'    :  'LRIS',
         'trigger'  :  'LOUTFILE',
-        'val'      :  'false',
-        'format'   :  '{OUTDIR}/{OUTFILE}{LFRAMENO}.fits',
-        'zfill'    :  {'LFRAMENO': 4},
+        'val'      :  None,
         'heartbeat':  ['UPTIME', 1],
         'transfer' : 0
     },
@@ -98,7 +96,7 @@ instr_keymap = {
         'val'      :  'false',
         'format'   :  '{OUTDIR}/{OUTFILE}{LFRAMENO}.fits',
         'zfill'    :  {'LFRAMENO': 4},
-        'probe':  '', # LRIS doesnt have any keywords to track uptime. LRIS upgrade should fix this
+        'heartbeat':  '', # LRIS doesnt have any keywords to track uptime. LRIS upgrade should fix this
         'transfer' : 0
     },
 
