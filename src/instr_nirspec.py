06f78f580d43d29772ca4be69780d4726f64d42c
Jeff Mader <jmader@vm-koaserver2.(none)>
2022-07-31 14:29:35 -1000

    Added self.log_warn(KOAIMTYP_UDF) for when KOAIMTYP is undefined

--- src/instr_nirspec.py
+++ src/instr_nirspec.py
@@ -208,6 +208,7 @@ class Nirspec(instrument.Instrument):
         #warn if undefined
         if (koaimtyp == 'undefined'):
             log.info('set_koaimtyp: Could not determine KOAIMTYP from OBSTYPE value')
+            self.log_warn("KOAIMTYP_UDF")
 
         #update keyword
         self.set_keyword('KOAIMTYP', koaimtyp, 'KOA: Image type')
