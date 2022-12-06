f305c22786c69f256e9dd15b6d07227d2c7eacdc
tylertucker202 <tylertucker202@gmail.com>
2022-12-05 14:55:04 -1000

    reworked report form

--- src/generate_nightly_report.py
+++ src/generate_nightly_report.py
@@ -4,6 +4,7 @@ from glob import glob
     
 import db_conn
 from datetime import datetime, timedelta
+from collections import Counter
 import pdb
 
 
@@ -14,17 +15,20 @@ from socket import gethostname
 from astropy.io import fits as fits
 import yaml
 
-def send_to_slack(body, instrument):
-
+def get_config():
     configPath = os.path.realpath(os.path.dirname(__file__) )
     configPath = os.path.join(configPath, 'config.live.ini')
     url = None
     if isfile(configPath):
         with open(configPath) as f:
             config = yaml.safe_load(f)
+    return config
 
-        url = config.get('API', {}).get('SLACKAPP', None)
-    if url != None:
+def send_to_slack(body, instrument, debug=False):
+
+    config = get_config()
+    url = config.get('API', {}).get('SLACKAPP', None)
+    if url != None and not debug:
         data = {}
         data['text'] = body
 
@@ -32,7 +36,7 @@ def send_to_slack(body, instrument):
         if msg.status_code != 200:
             print('Error sending message to Slack')
     else:
-        print('Error sending message to Slack - no URL')
+        print(body)
 
 def get_database():
     configPath = os.path.realpath(os.path.dirname(__file__) )
@@ -59,24 +63,43 @@ def get_daily_table(db, date, endDate, instr, level=0):
 
 def get_database_metrics(rows):
     ofname = [x.get('ofname') for x in rows]
+    reviewed = [x.get('reviewed') for x in rows]
     incomplete = [x for x in rows if x['status'].upper()=='INCOMPLETE']
     error = [x for x in rows if x['status'].upper()=='ERROR']
     invalid = [x for x in rows if x['status'].upper()=='INVALID']
+    statusCodes = [x.get('status_code') for x in rows]
+    notReviewedStatusCodes = [ statusCodes[idx] for idx in \
+                              range(len(statusCodes)) if reviewed[idx]==0 ]
+    uniqueStatusCodes = dict(Counter(notReviewedStatusCodes).items())
+    if '' in uniqueStatusCodes.keys():
+        del uniqueStatusCodes['']
+
 
     dirs = [ "/".join(x.split('/')[0:-1]) for x in ofname]
     dirs = [*set(dirs)]
 
+    basenames = [ x.split('/')[-1][0:5] for x in ofname]
+    basenames = [*set(basenames)]
+
     numIncomplete = len(incomplete) 
     numError = len(error)
     numInvalid = len(invalid)
-    return (dirs, numIncomplete, numError, numInvalid)
+    return (dirs, 
+            reviewed,
+            uniqueStatusCodes, 
+            numIncomplete, 
+            numError, 
+            numInvalid, 
+            basenames)
     
 
 def count_dir_files(dirs, startDate, endDate):
     files = []
 
-    startTimestamp = datetime.timestamp(startDate)
-    endTimestamp  = datetime.timestamp(endDate)
+    startHIDate = startDate - timedelta(hours=10)
+    endHIDate  = endDate   - timedelta(hours=10)
+    startTimestamp = datetime.timestamp(startHIDate)
+    endTimestamp  = datetime.timestamp(endHIDate)
 
     def within_range(filePath):
         ts = os.path.getctime(filePath)
@@ -134,55 +157,83 @@ def count_nirspec_dir_files(dirs, startDate, endDate):
 
 def get_missing_files(filesDict, rows):
     
-    ofnames = [x['ofname'] for x in rows]
+    ofnames = [x['ofname'].replace('//', '/') for x in rows]
     missingFiles = []
     for file in filesDict['files']:
-        if not file in ofnames:
+        if not file.replace('//','/') in ofnames:
             missingFiles.append(file)
     return missingFiles
+
+def filter_out_basenames(missingFiles, basenames):
+    filteredFiles = [fileName for fileName in missingFiles\
+                              for basename in basenames\
+                              if basename in fileName ]
+    return filteredFiles
 
 def make_report(instrument, 
                 date, 
                 dirs, 
+                reviewed,
+                uniqueStatusCodes, 
                 numIncomplete, 
                 numError, 
                 numInvalid,
+                basenames,
                 numDirFiles, 
+                scheduled,
                 numLev0DBFiles,
                 numLev1DBFiles,
                 numLev2DBFiles,
                 missingFiles):
     strDirs = ",\n\t".join(dirs)
+
+    dateStr = datetime.strftime(date, '%Y-%m-%d')
+    isScheduled = 'Yes' if scheduled else 'No'
+
+    numFilesTotal = numDirFiles.get('numFiles')
+    numDBFiles = numLev0DBFiles + numLev1DBFiles + numLev2DBFiles
+    if numDBFiles==0:
+        percentComplete=0
+    else:
+        percentComplete = round( 100 * (numDBFiles - numIncomplete - numError - \
+                                        numInvalid)/ numDBFiles )
+
     report = f"""\n
     Instrument:\t {instrument}
-    UT date:\t {date}
-    Number of incomplete ingests:\t {numIncomplete}
-    Number of errored ingests:\t {numError}
-    Number of invalid ingests:\t {numInvalid}
+    UT date:\t {dateStr}
+    Scheduled?:\t {isScheduled}
+    *Number of FITS in OUTDIRs:*\t {numFilesTotal}
+    *Number of ingestions:*\t {numDBFiles}
+    *% complete:*\t {percentComplete}
     Data directories:\n\t {strDirs}
     """
+
+    status_code_report = "Status_Code Errors/Warnings:\n"
+
+    for key, val in uniqueStatusCodes.items():
+        status_code_report += f'{key}:\t {val}\n'
+
+    if uniqueStatusCodes and not reviewed:
+        report += status_code_report
+
     if instrument.upper() == 'NIRSPEC':
         numNspec = numDirFiles.get('numNspec', False)
         numNscam= numDirFiles.get('numNscam', False)
         if numNspec:
-            report += f"""Number of nspec files in Directories:\t {numNspec}\n"""
+            report += f"""Number of nspec FITS in OUTDIRS:\t {numNspec}\n"""
         if numNscam:
-            report += f"""    Number of nscam files in Directories:\t {numNscam}\n"""
+            report += f"""    Number of nscam FITS in OUTDIRS:\t {numNscam}\n"""
 
-    numFilesTotal = numDirFiles.get('numFiles')
-    numDBFiles = numLev0DBFiles + numLev1DBFiles + numLev2DBFiles
-    report += f"""
-    lev0 files in Database:\t {numLev0DBFiles}
-    lev1 files in Database:\t {numLev1DBFiles}
-    lev2 files in Database:\t {numLev2DBFiles}
-    *Total number of files in Directories:*\t {numFilesTotal}
-    *Total number of files in Database:*\t {numDBFiles}
-    """
-
-    strMissingFiles = ",\n\t".join(missingFiles)
+    missingFiles.sort()
+    if len(missingFiles) > 50:
+        missingFiles = missingFiles[0:49]
+        strMissingFiles = ",\n\t".join(missingFiles[0:49]).replace(' ', '')
+        strMissingFiles += "\n\t and more..."
+    else:
+        strMissingFiles = ",\n\t".join(missingFiles).replace(' ', '')
     warnMsg = f"""
     =================================
-    Warning: non ingested files found 
+    Error: non ingested files found: {len(missingFiles)}
     =================================
     {strMissingFiles}
     """
@@ -192,34 +243,37 @@ def make_report(instrument,
     =================================
     Incomplete: Found incomplete ingests: {numIncomplete}
     =================================
+    Number of incomplete ingests:\t {numIncomplete}
     """
 
     invalidMsg = f"""
     =================================
     Invalid: Found invalid files: {numInvalid} 
     =================================
+    Number of invalid ingests:\t {numInvalid}
     """
 
     errMsg = f"""
     =================================
     Error: Found errored files: {numError}
     =================================
+    Number of errored ingests:\t {numError}
     """
 
-    level = 'INFO'
+    level = 'INFO :relieved:'
 
     if len(missingFiles) > 0: 
         report += warnMsg
-        level = 'WARNING'
+        level = ':fire::fire::fire:CRITICAL:fire::fire::fire:'
     if numIncomplete > 0:
         report += incompleteMsg
-        level = 'WARNING'
+        level = ':warning:WARNING:warning:'
     if numInvalid> 0:
         report += invalidMsg 
-        level = 'WARNING'
+        level = ':warning:WARNING:warning:'
     if numError > 0:
         report += errMsg
-        level = 'CRITICAL'
+        level = ':fire::fire::fire:CRITICAL:fire::fire::fire:'
     report = f"\nLevel: {level}" + report
 
     return report 
@@ -241,7 +295,8 @@ def get_associated_fcs_files(filesDict, rows, missingFiles):
 
         
 
-def generate_report(instrument, date):
+def generate_report(instrument, date, debug=False):
+    scheduled = is_instrument_scheduled(date, instrument)
     db = get_database()
     lev0Rows = get_daily_table( db, date, endDate, instrument, level=0 )
     numLev0DBFiles = len( lev0Rows )
@@ -252,95 +307,69 @@ def generate_report(instrument, date):
 
     rows = [*lev0Rows, *lev1Rows, *lev2Rows]
     metrics = get_database_metrics(rows)
+    dirs = metrics[0]
 
-    filesDict = count_nirspec_dir_files(metrics[0], date, endDate) \
+    basenames = metrics[-1]
+
+
+    filesDict = count_nirspec_dir_files(dirs, date, endDate) \
             if instrument.upper() == 'NIRSPEC' \
-            else count_dir_files(metrics[0], date, endDate)
+            else count_dir_files(dirs, date, endDate)
 
     missingFiles = get_missing_files(filesDict, rows)
+
+    missingFiles = filter_out_basenames(missingFiles, basenames)
 
     if instrument.upper() == 'DEIMOS':
         whitelist = get_associated_fcs_files(filesDict, rows, missingFiles)
         # remove referenced fcs files from missingFile list
         missingFiles = [ x for x in missingFiles if x[2:] in whitelist ]
 
+    missingFiles = [*set(missingFiles)]
+
     report = make_report(instrument,
                          date, 
                          *metrics,
                          filesDict, 
+                         scheduled,
                          numLev0DBFiles,
                          numLev1DBFiles,
                          numLev2DBFiles,
                          missingFiles
                         )
 
-    send_to_slack(report, instrument.upper())
+    numFilesTotal = filesDict.get('numFiles')
+    # ignore non scheduled and silent instruments
+    if not scheduled and numFilesTotal==0: 
+        print('inst not scheduled and silent. not sending to slack.')
+        send_to_slack(report, instrument.upper(), True)
+    else:
+        send_to_slack(report, instrument.upper(), debug)
 
-def get_instruments(date, dome):
-    url = f"https://www.keck.hawaii.edu/software/db_api/telSchedule.php?cmd=getSchedule&date={datetime.strftime(date, '%Y-%m-%d')}"
+def is_instrument_scheduled(date, inst):
+    config = get_config()
+    telApi = config.get('API', {}).get('TELAPI')
+    telSchedDate = date - timedelta(days=1) # tel sched uses HT
+    dateStr = datetime.strftime(telSchedDate, '%Y-%m-%d')
+    if inst.upper()=='NIRSPEC':
+        inst = 'NIRSP'
+    url = f"{telApi}cmd=getSchedule&date={dateStr}&instr={inst}"
     resp = requests.get(url)
-    schedInstruments = [ x.get('Instrument') for x in resp.json() if x.get('Instrument', False)]
-    schedInstruments = [*set(schedInstruments)]
-    ignoreList = ['SSC', 'PCS', 'PCS+SSC']
-    schedInstruments = [ x for x in schedInstruments if not x in ignoreList ]
-
-
-    schedInstNames = []
-    for schedInst in schedInstruments:
-        for k, v in INSTRUMENT_MAP.items():
-            if schedInst in v:
-                schedInstNames.append(k)
-                break
-
-    schedInstNames = [ x for x in schedInstNames if x in DOME_INSTRUMENTS[dome] ] 
-    return schedInstNames 
-    
-def get_instrument_list():
-    url = lambda x: f"https://www.keck.hawaii.edu/software/db_api/telSchedule.php?cmd=getSchedule&date={datetime.strftime(x, '%Y-%m-%d')}"
-
-    date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    dates = [ date - timedelta(days=x) for x in range(400) ] 
-    instruments=[]
-    for date in dates: 
-        resp = requests.get(url(date))
-        dayInst = [ x.get('Instrument') for x in resp.json() if x.get('Instrument', False) ]
-        instruments = [*instruments,  *dayInst]
-
-    return [*set(instruments)] # return unique values
-
-INSTRUMENT_MAP = {
-    'NIRC2': ['NIRC2-LGS', 'NIRC2-NGS', 'NIRC2-PY'],
-    'NIRSPEC': ['NIRSPAO-NGS', 'NIRC2-PY+NIRSPAO', 'NIRSPAO+LGS', 'NIRC2-NGS+NIRSPAO', 'NIRSPEC'],
-    'ESI': ['ESI'],
-    'KPF': ['KPF'],
-    'SSC': ['SSC'],
-    'MOSFIRE': ['MOSFIRE'],
-    'NIRES': ['NIRES'], 
-    'LRIS': ['LRIS-ADC'],
-    'OSIRIS': ['OSIRIS-LGS, OSIRIS-NGS'],
-    'KCWI': ['KCWI'],
-    'PCS': ['PCS', 'PCS+SSC'],
-    'DEIMOS': ['DEIMOS'],
-    'HIRES': ['HIRESr', 'HIRESb'],
-}
-
-DOME_INSTRUMENTS = {
-    'K2': [ 'KCWI', 'DEIMOS', 'ESI', 'NIRC2', 'NIRES', 'NIRESPEC' ],
-    'K1': [ 'HIRES', 'OSIRIS', 'MOSFIRE', 'KPF', 'LRIS' ]
-}
+    sched = resp.json() # either a list or empty dict
+    if len(sched)>0:
+        return True
+    return False
 
 if __name__ == '__main__':
     TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
     parser = argparse.ArgumentParser(description='generate_nightly_report input parameters')
     parser.add_argument('--instrument', type=str, help='instrument to check that generates report')
     parser.add_argument('--date', type=str, help='date', default=None)
-    parser.add_argument('--dome', type=str, help='date', default=None)
+    parser.add_argument('--dome', type=str, help='dome is K1 or K2', default=None)
+    parser.add_argument('--debug', type=bool, help='debug prints out report instead of sending it to Slack.', default=False)
 
     args = parser.parse_args()
-    dome = args.dome
-    if not dome:
-        hostname = os.uname()[1]
-        dome = 'K1' if hostname == 'vm-koarti' else 'K2'
+    debug = args.debug
 
     dateStr = args.date
     if dateStr:
@@ -351,8 +380,13 @@ if __name__ == '__main__':
     instrument = args.instrument
     
     if instrument:
-        generate_report(instrument, date)
+        generate_report(instrument, date, debug)
     else:
-        instruments = get_instruments(date, dome)
-        [generate_report(x, date) for x in instruments]
+        dome = args.dome
+        if not dome:
+            hostname = os.uname()[1]
+            dome = 'K1' if hostname == 'vm-koarti' else 'K2'
+        config = get_config()
+        instruments = config.get(dome.upper(), [])
+        [generate_report(x, date, debug) for x in instruments]
 
