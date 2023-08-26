a3ce356d939e4d923cba5eb73dee4d54d8830086
joh33-wmko <jhayashi@keck.hawaii.edu>
2023-08-25 16:51:59 -1000

    replaces default values for FILTER0 and FILTER1 with the word blank in combined keyword FILTER

--- src/instr_guider.py
+++ src/instr_guider.py
@@ -322,7 +322,10 @@ class Guider(instrument.Instrument):
 
         filter0 = self.get_keyword('FILTER0', default='')
         filter1 = self.get_keyword('FILTER1', default='')
-        filter = '+'.join(filter(None(filter0, filter1)))
+        if filter0 == '' and filter1 == '':
+            filter = 'blank'
+        else:
+            filter = '+'.join(filter(None(filter0, filter1)))
 
         #update keyword
         self.set_keyword('FILTER', filter, 'KOA: set from FILTER0 and FILTER1')
