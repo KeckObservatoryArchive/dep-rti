56b861eff87ba5ed0e7697fe7978cc86433223f0
Jeff Mader <jeff.mader@gmail.com>
2026-08-21 08:52:18 -0700

    Removing KOAIMTYP_UDF warning

--- src/instr_osiris.py
+++ src/instr_osiris.py
@@ -132,7 +132,7 @@ class Osiris(instrument.Instrument):
         # warn if undefined
         if koaimtyp == 'undefined':
             log.info('set_koaimtyp: Could not determine KOAIMTYP value')
-            self.log_warn("KOAIMTYP_UDF")
+            #self.log_warn("KOAIMTYP_UDF")
 
         # update keyword
         self.set_keyword('KOAIMTYP', koaimtyp, 'KOA: Image type')
